87ca69e3650a59f45283a93edb8c1ba362df0f1c
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-07-02 20:12:06 +0200

    init orphan branch gh-pages with gitignore and Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var grunt = require('grunt');
+
+module.exports = function () {
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.initConfig({
+    clean: {
+      release: ['./index.html', '*/*', '!node_modules/*']
+    },
+    copy: {
+      release: {
+        files: [{
+          expand: true,
+          cwd: './../w11k-flash-page/build-output/compiled',
+          src: ['**'],
+          dest: './'
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['release']);
+  grunt.registerTask('release', ['clean:release', 'copy:release']);
+};
