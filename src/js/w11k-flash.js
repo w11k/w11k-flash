a306b6b7e6e81e18473ba5ecbe3255bc609b18e8
colmaengus <colmaengus@users.noreply.github.com>
2015-03-25 15:15:33 +0000

    Avoid use of angular.element.scope()
    scope lookup doesn't work when using debugInfoEnabled(false) so attach scope to the lookup table.

--- src/js/w11k-flash.js
+++ src/js/w11k-flash.js
@@ -47,7 +47,7 @@ angular.module('w11k.flash').run(['$window', 'w11kFlashRegistry', function ($win
     $window.w11kFlashCall = function (flashId, expression, locals) {
       var flash = w11kFlashRegistry.getFlash(flashId);
       if (angular.isDefined(flash)) {
-        var scope = flash.element.scope();
+        var scope = flash.scope;
 
         // we have to evaluate the expression outside of an apply-function,
         // otherwise we are unable to return the result to flash
@@ -135,7 +135,8 @@ angular.module('w11k.flash').directive('w11kFlash', ['swfobject', '$window', '$q
 
       w11kFlashRegistry.registerFlash(flashId, {
         deferred: deferred,
-        element: element
+        element: element,
+        scope: scope
       });
 
       scope.$on('$destroy', function () {
